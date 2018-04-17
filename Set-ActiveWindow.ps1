c35e91c53a50c4ebdda63f2fa380730a14307646
AmomentOfMusic <38445436+AmomentOfMusic@users.noreply.github.com>
2018-04-16 21:55:07 -0400

    Add files via upload

---
+++ Set-ActiveWindow.ps1
@@ -0,0 +1,32 @@
+[CmdletBinding()]
+Param(
+)
+Add-Type @"
+  using System;
+  using System.Runtime.InteropServices;
+  public class Zotero {
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool SetForegroundWindow(IntPtr hWnd);
+}
+"@
+add-type -AssemblyName microsoft.VisualBasic
+add-type -AssemblyName System.Windows.Forms
+
+try {
+  $ScrivenerHandle = (Get-Process Scrivener).MainWindowHandle
+  $ref = invoke-expression "curl 'http://localhost:23119/better-bibtex/cayw?format=scannable-cite'"   
+  [Microsoft.VisualBasic.Interaction]::AppActivate((Get-Process Scrivener).ID)
+
+  $SendKeysSpecialChars = '{','}','[',']','~','+','^','%'
+  $ToEscape = ($SendKeysSpecialChars|%{[regex]::Escape($_)}) -join '|'
+  $citation = $ref.Content -replace "($ToEscape)",'{$1}'
+  echo "$citation"
+
+  [System.Windows.Forms.SendKeys]::SendWait($citation)
+  [Zotero]::SetForegroundWindow($ScrivenerHandle)
+  
+
+} catch {
+ Write-Error "Failed to get active Window details. More Info: $_"
+}
---
+++ zotpick-pandoc-win.bat
@@ -0,0 +1,1 @@
+@start /b powershell -ExecutionPolicy Unrestricted ./Set-ActiveWindow.ps1
