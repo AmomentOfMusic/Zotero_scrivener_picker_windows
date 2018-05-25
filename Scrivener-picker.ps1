e63246aaf211abaa7c191f69b4559122d3656e85
Emilie <38445436+AmomentOfMusic@users.noreply.github.com>
2018-05-25 17:49:15 -0400

    Update Scrivener-picker.ps1
    Added autoolt commands to fix issue with Zotero picker not always coming to the front.

--- Scrivener-picker.ps1
+++ Scrivener-picker.ps1
@@ -10,23 +10,31 @@ Add-Type @"
     public static extern bool SetForegroundWindow(IntPtr hWnd);
 }
 "@
+
 add-type -AssemblyName microsoft.VisualBasic
 add-type -AssemblyName System.Windows.Forms
 
 try {
-  $ScrivenerHandle = (Get-Process Scrivener).MainWindowHandle
-  $ref = invoke-expression "curl 'http://localhost:23119/better-bibtex/cayw?format=scannable-cite' -usebasicparsing"   
+  
+  [Microsoft.VisualBasic.Interaction]::AppActivate((Get-Process zotero).ID)
   [Microsoft.VisualBasic.Interaction]::AppActivate((Get-Process Scrivener).ID)
 
-  $SendKeysSpecialChars = '@','{','}','[',']','~','+','^','%','.','(',')','?'
+  $ref = invoke-expression "curl 'http://localhost:23119/better-bibtex/cayw?format=scannable-cite'-usebasicparsing"
+  
+  $ScrivenerHandle = (Get-Process Scrivener).MainWindowHandle
+
+  $SendKeysSpecialChars = '@','{','}','[',']','~','+','^','%','.','(',')','?','.',':'
   $ToEscape = ($SendKeysSpecialChars|%{[regex]::Escape($_)}) -join '|'
   $citation = $ref.Content -replace "($ToEscape)",'{$1}'
   echo "$citation"
 
   [Zotero]::SetForegroundWindow($ScrivenerHandle)
+  Show-AU3WinActivate($ScrivenerHandle)
+  Wait-AU3WinActive($ScrivenerHandle)
+
   [System.Windows.Forms.SendKeys]::SendWait($citation)
   
-
+  
 } catch {
  Write-Error "Failed to get active Window details. More Info: $_"
 }
