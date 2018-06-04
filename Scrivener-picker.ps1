e30c74ad8bce0f77ea6ae5a4c07c935fd45f50b7
Emilie <38445436+AmomentOfMusic@users.noreply.github.com>
2018-06-04 19:39:23 -0400

    Update Scrivener-picker.ps1
    Add function to check if Zotero is running and if not, launch it minimized

--- Scrivener-picker.ps1
+++ Scrivener-picker.ps1
@@ -14,26 +14,43 @@ Add-Type @"
 add-type -AssemblyName microsoft.VisualBasic
 add-type -AssemblyName System.Windows.Forms
 
+if ((get-process "zotero" -ErrorAction SilentlyContinue) -eq $Null)
+   {
+        Start-Process -filepath zotero.exe -windowstyle Minimized
+    }
+
+else
+    { 
+        echo "Process is running" 
+     }
+   
+$probe = Invoke-Expression "curl 'http://localhost:23119/better-bibtex/cayw?probe=true' -usebasicparsing"
+echo $probe
+ 
+ while ($probe -notmatch "ready") {
+        start-sleep -Seconds 2
+        $probe = invoke-expression "curl 'http://localhost:23119/better-bibtex/cayw?probe=true' -usebasicparsing'"
+    }
+    
 try {
   
   [Microsoft.VisualBasic.Interaction]::AppActivate((Get-Process zotero).ID)
-  [Microsoft.VisualBasic.Interaction]::AppActivate((Get-Process Scrivener).ID)
-
   $ref = invoke-expression "curl 'http://localhost:23119/better-bibtex/cayw?format=scannable-cite'-usebasicparsing"
   
-  $ScrivenerHandle = (Get-Process Scrivener).MainWindowHandle
+  [Microsoft.VisualBasic.Interaction]::AppActivate((Get-Process Scrivener).ID)
 
   $SendKeysSpecialChars = '@','{','}','[',']','~','+','^','%','.','(',')','?','.',':'
   $ToEscape = ($SendKeysSpecialChars|%{[regex]::Escape($_)}) -join '|'
   $citation = $ref.Content -replace "($ToEscape)",'{$1}'
   echo "$citation"
 
+ $ScrivenerHandle = (Get-Process Scrivener).MainWindowHandle
+
   [Zotero]::SetForegroundWindow($ScrivenerHandle)
   Show-AU3WinActivate($ScrivenerHandle)
   Wait-AU3WinActive($ScrivenerHandle)
 
   [System.Windows.Forms.SendKeys]::SendWait($citation)
-  
   
 } catch {
  Write-Error "Failed to get active Window details. More Info: $_"
